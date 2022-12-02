3e90bec148735a6f617314c1f0e263d47ae38d45
elektrike <solargraphs@gmail.com>
2022-12-02 17:42:52 +0200

    day 2

--- src/main/scala/Day2.scala
+++ src/main/scala/Day2.scala
@@ -1,4 +1,4 @@
-import Day2.Hand.{Paper, Rock, Scissors}
+import Day2.Hand.{Rock, Paper, Scissors}
 import Day2.Outcome.{Lose, Draw, Win}
 import util.readFileLines
 
@@ -10,21 +10,13 @@ object Day2:
   enum Outcome:
     case Lose, Draw, Win
 
-  val rules: Map[Hand, Hand] =
-    Map(Scissors -> Paper, Paper -> Rock, Rock -> Scissors)
+  val toHand = Map('A' -> Rock, 'B' -> Paper, 'C' -> Scissors, 'X' -> Rock, 'Y' -> Paper, 'Z' -> Scissors)
 
-  val handDecrypted: Map[Char, Hand] =
-    Map('A' -> Rock, 'B' -> Paper, 'C' -> Scissors,
-        'X' -> Rock, 'Y' -> Paper, 'Z' -> Scissors)
+  val toOutcome = Map('X' -> Lose, 'Y' -> Draw, 'Z' -> Win)
 
-  val outcomeDecrypted: Map[Char, Outcome] =
-    Map('X' -> Lose, 'Y' -> Draw, 'Z' -> Win)
+  val beats = Map(Scissors -> Paper, Paper -> Rock, Rock -> Scissors)
 
-  val handScore: Map[Hand, Int] =
-    Map(Rock -> 1, Paper -> 2, Scissors -> 3)
-
-  val outcomeScore: Map[Outcome, Int] =
-    Map(Lose -> 0, Draw -> 3, Win -> 6)
+  val toPoints = Map(Rock -> 1, Paper -> 2, Scissors -> 3, Lose -> 0, Draw -> 3, Win -> 6)
 
   def solution(): (Int, Int) =
     val guide = readFileLines("input/day2.txt")
@@ -32,37 +24,33 @@ object Day2:
     (partOne(guide), partTwo(guide))
 
   def partOne(guide: List[String]): Int =
-    def decrypt(line: String): (Hand, Hand) =
-      (handDecrypted(line.head), handDecrypted(line.last))
+    def decrypt(row: String): (Hand, Hand) =
+      (toHand(row.head), toHand(row.last))
 
-    guide
-      .map(decrypt andThen play andThen score)
-      .sum
+    guide.map(decrypt andThen play andThen score).sum
 
   def partTwo(guide: List[String]): Int =
-    def decrypt(line: String): (Hand, Hand) =
-      val opponentHand = handDecrypted(line.head)
-      val outcome = outcomeDecrypted(line.last)
+    def decrypt(row: String): (Hand, Hand) =
+      val opponentHand = toHand(row.head)
+      val outcome      = toOutcome(row.last)
 
       (opponentHand, chooseHand(opponentHand, outcome))
 
-    guide
-      .map(decrypt andThen play andThen score)
-      .sum
+    def chooseHand(opponentHand: Hand, outcome: Outcome): Hand =
+      outcome match
+        case Lose => beats(opponentHand)
+        case Win  => beats.map(_.swap)(opponentHand)
+        case Draw => opponentHand
 
-  def chooseHand(opponentHand: Hand, outcome: Outcome): Hand =
-    outcome match
-      case Lose => rules(opponentHand)
-      case Win => rules.map(_.swap)(opponentHand)
-      case Draw => opponentHand
+    guide.map(decrypt andThen play andThen score).sum
 
   def play(hands: (Hand, Hand)): (Outcome, Hand) =
-    (resolveRound(hands), hands._2)
+    val outcome =
+      if      (hands._1 == hands._2)        Draw
+      else if (hands._1 == beats(hands._2)) Win
+      else                                  Lose
 
-  def resolveRound(hands: (Hand, Hand)): Outcome =
-    if (hands._1 == hands._2) Draw
-    else if (hands._1 == rules(hands._2)) Win
-    else Lose
+    (outcome, hands._2)
 
   def score(round: (Outcome, Hand)): Int =
-    outcomeScore(round._1) + handScore(round._2)
+    toPoints(round._1) + toPoints(round._2)
