e57d1e3ce44eb45012a629b47dd885c9126514e3
elektrike <solargraphs@gmail.com>
2022-12-02 17:20:13 +0200

    day 1 (formatting)

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -1,12 +1,11 @@
 import util.readBatches
 
-object Day1 {
+object Day1:
 
-  def solution(): (Int, Int) = {
+  def solution(): (Int, Int) =
     val inventory = toInventory("input/day1.txt")
 
     (partOne(inventory), partTwo(inventory))
-  }
 
   def toInventory(path: String): List[Int] =
     readBatches(path).map(_.split("\n").map(_.toInt).sum)
@@ -16,4 +15,3 @@ object Day1 {
 
   def partTwo(inventory: List[Int]): Int =
     inventory.sortWith(_ > _).take(3).sum
-}
