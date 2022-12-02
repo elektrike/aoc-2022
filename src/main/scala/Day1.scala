b999fcb54eb3f613f50505e0e0a9f37479e44667
elektrike <solargraphs@gmail.com>
2022-12-02 17:40:26 +0200

    day 1

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -3,15 +3,21 @@ import util.readBatches
 object Day1:
 
   def solution(): (Int, Int) =
-    val inventory = toInventory("input/day1.txt")
+    val inventory = readBatches("input/day1.txt").map(sumCalories)
 
     (partOne(inventory), partTwo(inventory))
 
-  def toInventory(path: String): List[Int] =
-    readBatches(path).map(_.split("\n").map(_.toInt).sum)
+  def sumCalories(elfInventory: String): Int =
+    elfInventory
+      .split("\n")
+      .map(_.toInt)
+      .sum
 
   def partOne(inventory: List[Int]): Int =
     inventory.max
 
   def partTwo(inventory: List[Int]): Int =
-    inventory.sortWith(_ > _).take(3).sum
+    inventory
+      .sortWith(_ > _)
+      .take(3)
+      .sum
